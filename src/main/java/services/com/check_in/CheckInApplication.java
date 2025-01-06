215f620abdd8d621f6549a34fe4828f9cd5e8612
vitorantonionne <vitor-antonionne@hotmail.com>
2025-01-06 16:28:32 -0300

    project init

---
+++ src/main/java/services/com/check_in/CheckInApplication.java
@@ -0,0 +1,13 @@
+package services.com.check_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/services/com/check_in/CheckInApplicationTests.java
@@ -0,0 +1,13 @@
+package services.com.check_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
