566963d5ca998a70f01af46ba8341e1d0da79c75
vitorantonionne <vitor-antonionne@hotmail.com>
2025-01-08 10:57:53 -0300

    create files controllers

---
+++ src/main/java/services/com/check_in/controllers/AttendeeController.java
@@ -0,0 +1,9 @@
+package services.com.check_in.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attendees")
+public class AttendeeController {
+}
